20bd498b5751940cbd843ac3297db8aba6c15a1c
sarpitmishra2006 <s.arpitmishra2006@gmail.com>
2026-07-25 21:27:07 +0530

    Time: 1 ms (99.89%), Space: 55.8 MB (32.02%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int element=0;
+        int count=0;
+        for(int num:nums){
+        if(count==0){
+            element=num;
+
+        }
+        //boyre-moore majority voting element
+        if(num==element){
+            count++;
+        }else{
+            count--;
+        }
+        
+
+    }
+    return element;
+    
+}
+}
