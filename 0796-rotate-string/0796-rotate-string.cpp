8cddca8c2c0f875e6ff782afa481e6175842223a
ARPIT MISHRA S <s.arpitmishra2006@gmail.com>
2026-09-18 11:06:26 +0530

    Time: 0 ms (100%), Space: 7.9 MB (83.46%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -2,15 +2,6 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
 
-        if(s.length() != goal.length()){
-            return false;
-        }
-        string concatenated=s+s;
-        if(concatenated.find(goal) != string::npos){
-            return true;
-        }else{
-            return false;
-        }
-    
+        return (s.size()==goal.size() && (s+s).contains(goal));
     }
 };
